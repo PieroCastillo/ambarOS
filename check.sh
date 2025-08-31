60738c3ce906aa345e03fa67eeac5a64d90a79b4
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2025-08-31 16:02:32 -0500

    .

--- check.sh
+++ check.sh
@@ -1,4 +1,5 @@
-if grub-file --is-x86-multiboot2 bin/volta.bin; then
+if grub-file --is-x86-multiboot2 bin/volta.bin
+then
   echo [+] Multiboot confirmed
 else
   echo [-] The file is not a valid multiboot file
